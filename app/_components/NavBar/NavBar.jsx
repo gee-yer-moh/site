3db6b6bc250ec6ced514cded386217f15b35c15e
Guillermo Avelar <guillermo@avelar.family>
2024-12-22 17:50:12 -0500

    Size fixes

--- app/_components/NavBar/NavBar.jsx
+++ app/_components/NavBar/NavBar.jsx
@@ -5,10 +5,10 @@ import Image from "next/image";
 export default function NavBar() {
   return (
     <div className={styles.container}>
-      <Image src={dodecagon} alt="dodecagon" width={20} height={20} />
+      <Image src={dodecagon} alt="dodecagon" width={25} height={25} />
       <div className={styles.links}>
-        <div className="b2">dark</div>
-        <div className="b2">español</div>
+        <div className="b3">dark</div>
+        <div className="b3">español</div>
       </div>
     </div>
   );
--- app/_components/Title/Title.jsx
+++ app/_components/Title/Title.jsx
@@ -4,7 +4,7 @@ export default function Title() {
   return (
     <div className={styles.container}>
       <h5>Guillermo Avelar</h5>
-      <div className={`b2 ${styles.pronunciation}`}>/gee-YER-moh  ah-veh-LAHR/</div>
+      <div className={`b1 ${styles.pronunciation}`}>/gee-YER-moh  ah-veh-LAHR/</div>
     </div>
   );
 }
