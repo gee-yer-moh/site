6046c161fb52ae15805c8ebd8f457c10c7b18b2f
Guillermo Avelar <guille@Guillermos-MBP.lan>
2024-12-22 17:32:31 -0500

    Config

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import NavBar from "./_components/NavBar";
+import Title from "./_components/Title";
 export default function Home() {
   return (
     <div className={styles.container}>
       <NavBar />
+      <Title />
     </div>
   );
 }
