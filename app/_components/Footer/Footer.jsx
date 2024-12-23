71fae1f07ba92d81cd6cd837050313c88468192c
Guillermo Avelar <guillermo@avelar.family>
2024-12-22 20:15:45 -0500

    Footer fixed

--- app/_components/Footer/Footer.jsx
+++ app/_components/Footer/Footer.jsx
@@ -2,9 +2,9 @@ import styles from './style.module.css';
 
 export default function Footer() {
     return <div className={styles.links}>
-            <div className="b3">@gee_yer_moh</div>
-            <div className="b3">github</div>
-            <div className="b3">linkedin</div>
-            <div className="b3">email</div>
+            <a className="b3" href="https://www.x.com/gee_yer_moh/" target="_blank">@gee_yer_moh</a>
+            <a className="b3" href="https://github.com/gee-yer-moh" target="_blank">github</a>
+            <a className="b3" href="https://www.linkedin.com/in/gee-yer-moh/" target="_blank">linkedin</a>
+            <a className="b3" href="mailto:jga2131@columbia.edu" target="_blank">email</a>
     </div>;
 }
