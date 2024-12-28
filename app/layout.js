72aff9bbf8bfc3df353a441f2a5c67abb6a8ab5e
Guillermo Avelar <guillermo@avelar.family>
2024-12-28 00:12:44 -0500

    Posthog setup

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import "./globals.css";
 
 import { Lora } from "next/font/google";
-
+import { CSPostHogProvider } from "./providers";
 const lora = Lora({
   subsets: ["latin"],
   display: "swap",
@@ -22,9 +22,11 @@ const fadeInStyle = (index) => ({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={lora.variable}>
-      <body>
-        {children}
+      <CSPostHogProvider>
+        <body>
+          {children}
         </body>
+      </CSPostHogProvider>
     </html>
   );
 }
